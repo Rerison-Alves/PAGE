ead7122c3ca4bb1487b9be019223485bc9b3d86c
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2022-06-20 14:57:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetopage/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.projetopage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button cima = (Button) findViewById(R.id.btn_cima);
+
+        cima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirtela2(view);
+            }
+        });
+
+    }
+
+    public void abrirtela2(View v){
+        Intent ittela2 = new Intent(this,Tela2.class);
+        startActivity(ittela2);
+    }
+}
