3b05ed3e995f8b29672d7b596d6f025dd73d4cbe
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2022-06-27 23:27:36 -0300

    Aba do perfil

--- app/src/main/java/com/example/projetopage/fragments/Perfil.java
+++ app/src/main/java/com/example/projetopage/fragments/Perfil.java
@@ -1,44 +1,33 @@
 package com.example.projetopage.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.projetopage.R;
+import com.example.projetopage.fragments.adapters.RecyclerViewAdapter;
+import com.example.projetopage.fragments.adapters.RecyclerViewAdapterExtended;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Perfil#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Perfil extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public Perfil() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Perfil.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Perfil newInstance(String param1, String param2) {
         Perfil fragment = new Perfil();
         Bundle args = new Bundle();
@@ -56,11 +45,23 @@ public class Perfil extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    String[] testeGpsUsuario= new String[] {"POO em aplicativos", "POO 2022", "Prog. Linear", "Nome de grupo", "Tô sem ideia"};
+    Context context;
+    RecyclerViewAdapterExtended recyclerViewAdapterExtended;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+        context= getContext();
+        //Lista de grupos
+        RecyclerView usuariogpsView = (RecyclerView) view.findViewById(R.id.gpsusuario);
+        RecyclerView.LayoutManager recycleLayoutUsuario = new LinearLayoutManager(context) ;
+        usuariogpsView.setLayoutManager(recycleLayoutUsuario);
+        recyclerViewAdapterExtended = new RecyclerViewAdapterExtended(context, testeGpsUsuario);
+        usuariogpsView.setAdapter(recyclerViewAdapterExtended);
+        usuariogpsView.setNestedScrollingEnabled( false );
+
+        return view;
     }
 }
